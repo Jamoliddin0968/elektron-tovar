03c79bc871e5c1b171fc5a5c8c9d455f51f99165
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-03-25 20:43:38 +0500

    dsF

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=31, default="qo'l soatlar")
+    name = models.CharField(max_length=31, default="soatlar")
     description = models.CharField(max_length=255, null=True, blank=True)
--- apps/products/models.py
+++ apps/products/models.py
@@ -8,7 +8,8 @@ class Product(models.Model):
     name = models.CharField(_("Nomi"), max_length=255)
     category = models.ForeignKey(
         'categories.Category', on_delete=models.CASCADE, related_name="category_products")
-    price = models.PositiveIntegerField(_("Sotish narxi"), default=0)
+    price = models.DecimalField(
+        _("Sotish narxi"), decimal_places=2, max_digits=13, default=0)
     description = models.TextField(_("Ma'lumot"))
 
     class Meta:
