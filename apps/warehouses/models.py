7941f523c56a169943055051b62a08ce648427fc
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-06-05 16:16:05 +0500

    add new view

--- apps/warehouses/models.py
+++ apps/warehouses/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class WareHouse(models.Model):
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     amount = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.product.name
--- apps/warehouses/views.py
+++ apps/warehouses/views.py
@@ -1,19 +1,22 @@
 from drf_spectacular.openapi import OpenApiParameter
 from drf_spectacular.types import OpenApiTypes
-
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import WareHouse
 from .serializers import WareHouseSerializer
 
-from drf_spectacular.utils import extend_schema, extend_schema_view
+
 @extend_schema_view(
-    list=extend_schema(tags=["Watehouse"]),
-    retrieve=extend_schema(tags=["Watehouse"]),
-    create=extend_schema(tags=["Watehouse"]),
-    update=extend_schema(tags=["Watehouse"]),
-    partial_update=extend_schema(tags=["Watehouse"]),
-    destroy=extend_schema(tags=["Watehouse"])
+    list=extend_schema(tags=["Warehouse"]),
+    retrieve=extend_schema(tags=["Warehouse"]),
+    create=extend_schema(tags=["Warehouse"]),
+    update=extend_schema(tags=["Warehouse"]),
+    partial_update=extend_schema(tags=["Warehouse"]),
+    destroy=extend_schema(tags=["Warehouse"]),
+    get_by_product_id=extend_schema(tags=["Warehouse"]),
 )
 class WareHouseViewSet(viewsets.ModelViewSet):
     queryset = WareHouse.objects.all()
@@ -29,3 +32,12 @@ class WareHouseViewSet(viewsets.ModelViewSet):
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
+
+    @action(detail=False, methods=['get'], url_path='product/(?P<product_id>[^/.]+)')
+    def get_by_product_id(self, request, product_id=None):
+        try:
+            warehouse = WareHouse.objects.get(product__id=product_id)
+            serializer = WareHouseSerializer(warehouse)
+            return Response(serializer.data)
+        except WareHouse.DoesNotExist:
+            return Response({'error': 'Warehouse entry not found for this product ID'}, status=404)
