20686a75e3a6d7ee813fe6721e5282e6618e13f0
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-07-10 09:56:38 +0500

    deleted price model

--- apps/info/views.py
+++ apps/info/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Info
 from .serializers import InfoSerializer
 
-
+@extend_schema_view(
+    list=extend_schema(tags=["Info"]),
+    retrieve=extend_schema(tags=["Info"]),
+    create=extend_schema(tags=["Info"]),
+    update=extend_schema(tags=["Info"]),
+    partial_update=extend_schema(tags=["Info"]),
+    destroy=extend_schema(tags=["Info"]),
+)
 class InfoModelViewSet(ModelViewSet):
     queryset = Info.objects.all()
     serializer_class = InfoSerializer
