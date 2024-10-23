a130de0878b59981745995502a02a276dc6dc985
Jamoliddin <jamoliddin0968@gmail.com>
2024-10-23 10:40:39 +0500

    add

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import UserDetailView, UserListView
 
 urlpatterns = [
-    path('users/me', UserDetailView.as_view(), name='user_detail'),
+    path('users/me/', UserDetailView.as_view(), name='user_detail'),
     path('api/users/', UserListView.as_view(), name='user_list'),
 ]
