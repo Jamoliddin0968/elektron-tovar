ed3763766d90c3c18a1a9b976beb7d68ebd2d6d9
Jamoliddin <jamoliddin0968@gmail.com>
2024-10-27 11:50:13 +0500

    add stck

--- config/settings.py
+++ config/settings.py
@@ -89,6 +89,7 @@ ROOT_URLCONF = 'config.urls'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
