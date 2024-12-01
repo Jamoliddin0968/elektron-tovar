c213a4a781e51c462af095ec1bea2e226f37bb44
Jamoliddin <jamoliddin0968@gmail.com>
2024-12-01 14:48:18 +0500

    fix error

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,7 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-from config.healtcheck import *
 import os
 
 from django.core.wsgi import get_wsgi_application
