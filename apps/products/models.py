130916436e25f83551576d9be228699c9095c00f
Jamoliddin <jamoliddin0968@gmail.com>
2024-12-15 11:19:22 +0500

    add new changes

--- apps/products/models.py
+++ apps/products/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
